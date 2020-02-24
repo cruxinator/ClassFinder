ce1bd55ac479bf5e6f3dbd311ebe55704e979de2
c-harris <c.harris@communicationinnovations.com.au>
2020-02-24 14:55:32 +1000

    cleanup comments

--- tests/Unit/ClassFinderTest.php
+++ tests/Unit/ClassFinderTest.php
@@ -3,9 +3,12 @@
 
 namespace Cruxinator\ClassFinder\Tests\Unit;
 
+use Composer\Autoload\ClassLoader;
 use Cruxinator\ClassFinder\ClassFinder;
 use Cruxinator\ClassFinder\Tests\ClassFinderConcrete;
 use Cruxinator\ClassFinder\Tests\TestCase;
+use Exception;
+use ReflectionClass;
 
 class ClassFinderTest extends TestCase
 {
@@ -19,6 +22,9 @@ class ClassFinderTest extends TestCase
         $this->classFinder = new ClassFinderConcrete();
     }
 
+    /**
+     * @throws Exception
+     */
     public function testSelf()
     {
         $classes = $this->classFinder->getClasses('Cruxinator\\ClassFinder\\');
@@ -27,6 +33,9 @@ class ClassFinderTest extends TestCase
         $this->assertTrue(in_array(TestCase::class, $classes));
     }
 
+    /**
+     * @throws Exception
+     */
     public function testFindPsr()
     {
         $classes = $this->classFinder->getClasses('Psr\\Log\\');
@@ -38,43 +47,56 @@ class ClassFinderTest extends TestCase
         $twoClasses = $this->classFinder->getClasses('Psr\\Log\\');
         $this->assertEquals(count($classes), count($twoClasses));
     }
-
+    /**
+     * @throws Exception
+     */
     public function testTwoCallsSameFinder()
     {
         $this->testFindPsr();
         $this->testSelf();
     }
 
+    /**
+     * @throws \ReflectionException
+     * @throws Exception
+     */
     public function testFindPsrNotAbstract()
     {
         $classes = $this->classFinder->getClasses('Psr\\Log\\', function ($class) {
-            $reflectionClass = new \ReflectionClass($class);
+            $reflectionClass = new ReflectionClass($class);
             return !$reflectionClass->isAbstract();
         });
         $this->assertTrue(count($classes) > 0);
         foreach ($classes as $class) {
             $this->assertTrue(class_exists($class));
             $this->assertStringStartsWith('Psr\\Log\\', $class);
-            $reflectionClass = new \ReflectionClass($class);
+            $reflectionClass = new ReflectionClass($class);
             $this->assertFalse($reflectionClass->isAbstract());
         }
     }
 
+    /**
+     * @throws \ReflectionException
+     * @throws Exception
+     */
     public function testFindPsrOnlyAbstract()
     {
         $classes = $this->classFinder->getClasses('Psr\\Log\\', function ($class) {
-            $reflectionClass = new \ReflectionClass($class);
+            $reflectionClass = new ReflectionClass($class);
             return $reflectionClass->isAbstract();
         });
         $this->assertTrue(count($classes) > 0);
         foreach ($classes as $class) {
             $this->assertTrue(class_exists($class));
             $this->assertStringStartsWith('Psr\\Log\\', $class);
-            $reflectionClass = new \ReflectionClass($class);
+            $reflectionClass = new ReflectionClass($class);
             $this->assertTrue($reflectionClass->isAbstract());
         }
     }
 
+    /**
+     * @throws Exception
+     */
     public function testFindPsrNotInVendor()
     {
         $classes = $this->classFinder->getClasses('Psr\\Log\\', null, false);
@@ -87,6 +109,9 @@ class ClassFinderTest extends TestCase
     public function testErrorCheck()
     {
         $unoptimised = $this->classFinder->classLoaderInit;
+        /**
+         * @var $autoloader ClassLoader
+         */
         $autoloader = $this->classFinder->getComposerAutoloader();
         $rawCM = $autoloader->getClassMap();
         foreach ($rawCM as $class => $file) {
@@ -98,7 +123,7 @@ class ClassFinderTest extends TestCase
             class_exists($class);
         }
         $this->assertFalse(class_exists('Composer\Autoload\ClassMapGenerator', false));
-        $dummyCL = new \Composer\Autoload\ClassLoader();
+        $dummyCL = new ClassLoader();
         $autoloader->unregister();
         unset($rawCM['Composer\Autoload\ClassMapGenerator']);
         $dummyCL->addClassMap($rawCM);
@@ -111,13 +136,13 @@ class ClassFinderTest extends TestCase
                 $this->fail('unoptimized autoloader should not get this far');
             }
             return;
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             $dummyCL->unregister();
             $autoloader->register();
             if (!$unoptimised) {
                 $this->fail('optimised class loader should not throw an exception');
             }
-            $this->assertInstanceOf(\Exception::class, $e);
+            $this->assertInstanceOf(Exception::class, $e);
             $this->assertStringContainsString('Cruxinator/ClassFinder', $e->getMessage());
             $this->assertStringContainsString('composer/composer', $e->getMessage());
             $this->assertStringContainsString('composer dump-autoload -o', $e->getMessage());
